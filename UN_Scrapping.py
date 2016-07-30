1a7d592fd59654665cf734f47a2538558653794c
dsunwoo <sunwoo.daniel@gmail.com>
2016-07-30 00:00:51 -0500

    Add partial code does not work

--- UN_Scrapping.py
+++ UN_Scrapping.py
@@ -7,17 +7,33 @@ url = "https://raw.githubusercontent.com/dsunwoo/UN/master/Scrape.html"
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
 
-# Initial table
-for index, row in enumerate(soup('table')):
-    print('table', index)
+# Use code below to scan through the table contents to find starting points
+"""
+for index, row in enumerate(soup(<index string>)):
+    print(<index string>, index)
     print(row)
-
-# Working table
+"""
+# Working table - Using above code, table index 11 is the starting point of the data
 main_table = soup('table')[11]
-# Find data in each row
-for index, row in enumerate(main_table('tr')):
-    if row.get_text().find("Country or area") != -1 and row.get_text().find("Year") != -1 and row.get_text().find("Total") != -1:
+header_row = main_table('tr')[7]
+# Scrape the header list
+header_cols = []
+data = {}
+for cell in header_row('td'):
+    header_cols.append(cell.get_text())
+# Replace the empty column names with NA
+# [('NA' if v is "" else v is v) for v in header_cols]
+# Scrape the data from table
+for col_name in header_cols:
+    for x in range(8, 190):
+        data_row = main_table('tr')[x]
+        for cell in data_row('td'):
+            data[col_name] = (cell.get_text())
+"""
+for row in main_table('tr'):
+    if row.get_text().find("Country or area") != -1 \
+            and row.get_text().find("Year") != -1 \
+            and row.get_text().find("Total") != -1:
         print('tr', index)
-        print(row.get_text())
-
-print(main_table('tr')[8])
+        print(row)
+"""
