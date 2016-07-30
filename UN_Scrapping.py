531ea0df79ebfd90dc2d527d2829fd2cde27490c
dsunwoo <sunwoo.daniel@gmail.com>
2016-07-30 13:48:41 -0500

    Fixed scraping and added dictionaries for DB prep

--- UN_Scrapping.py
+++ UN_Scrapping.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import sqlite3 as lite
+import pandas as pd
+import numpy as np
 
 # URL for the UN data set
 url = "https://raw.githubusercontent.com/dsunwoo/UN/master/Scrape.html"
@@ -16,24 +19,44 @@ for index, row in enumerate(soup(<index string>)):
 # Working table - Using above code, table index 11 is the starting point of the data
 main_table = soup('table')[11]
 header_row = main_table('tr')[7]
-# Scrape the header list
-header_cols = []
-data = {}
-for cell in header_row('td'):
-    header_cols.append(cell.get_text())
-# Replace the empty column names with NA
-# [('NA' if v is "" else v is v) for v in header_cols]
+
+# Setting up dictionaries
+d_year = {}
+d_total = {}
+d_men = {}
+d_women = {}
+
 # Scrape the data from table
-for col_name in header_cols:
-    for x in range(8, 190):
-        data_row = main_table('tr')[x]
-        for cell in data_row('td'):
-            data[col_name] = (cell.get_text())
-"""
-for row in main_table('tr'):
-    if row.get_text().find("Country or area") != -1 \
-            and row.get_text().find("Year") != -1 \
-            and row.get_text().find("Total") != -1:
-        print('tr', index)
-        print(row)
-"""
+country_list = []
+for x in range(8, 191):
+    data_row = main_table('tr')[x]
+    data = []
+    for cell in data_row('td'):
+        data.append(cell.get_text())
+    # Populate dictionaries with appropriate cell values
+    country_list.append(data[0])
+    d_year[data[0]] = data[1]
+    d_total[data[0]] = data[4]
+    d_men[data[0]] = data[7]
+    d_women[data[0]] = data[10]
+
+# Setup database
+col_names = ('Country', 'Year', 'Discard1', 'Discard2', 'Total', 'Men', 'Women')
+con = lite.connect('education.db')
+cur = con.cursor()
+with con:
+    cur.executescript('DROP TABLE IF EXISTS school_life')
+    cur.execute('CREATE TABLE school_life '
+                '(Country STRING, '
+                'Year INT, '
+                'Discard1 STRING, '
+                'Discard2 STRING, '
+                'Total INT, '
+                'Men INT, '
+                'Women INT);'
+                )
+# Create an empty dataset
+
+
+con.close
+
