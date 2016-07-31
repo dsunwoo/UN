df5ebca5ba8a43f9de020cb468a432d086ed4515
dsunwoo <sunwoo.daniel@gmail.com>
2016-07-31 15:23:59 -0500

    Update code for GDP data pull. Separate DataRead.py code

---
+++ UN_DataRead.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3 as lite
+import pandas as pd
+import numpy as np
+
+# Connect and setup database
+con = lite.connect('education.db')
+cur = con.cursor()
+
+# place gdp table into a dataframe
+df = pd.read_sql_query("SELECT * FROM gdp", con)
+
+gdp_tall = []
+
+for i in range(0, len(df)-1):
+    for k,v in df.items():
+        if k != 'country_name':
+            gdp_tall.append([df['country_name'][i], k, df[k][i]])
--- UN_Scrapping.py
+++ UN_Scrapping.py
@@ -11,6 +11,7 @@ for index, row in enumerate(soup(<index string>)):
 from bs4 import BeautifulSoup
 import requests
 import sqlite3 as lite
+import csv
 import pandas as pd
 import numpy as np
 
@@ -34,12 +35,15 @@ for x in range(8, 191):
     # Create a list of values for datatable
     data_list.append([data[0], int(data[1]), int(data[4]), int(data[7]), int(data[10])])
 
-# Setup database
+# Connect and setup database
 con = lite.connect('education.db')
 cur = con.cursor()
 with con:
+    # Check for existing tables and remove
     cur.executescript('DROP TABLE IF EXISTS school_life')
     cur.executescript('DROP TABLE IF EXISTS gdp')
+    cur.executescript('DROP TABLE IF EXISTS school_gdp')
+    # Initiate the School life expectancy table
     cur.execute('CREATE TABLE school_life '
                 '(Country TEXT, '
                 'Year INT, '
@@ -47,22 +51,38 @@ with con:
                 'Men INT, '
                 'Women INT);'
                 )
+    # Initiate the GDP table
     cur.execute('CREATE TABLE gdp'
                 '(country_name TEXT,'
                 '_1999 REAL, _2000 REAL, _2001 REAL, _2002 REAL,'
                 '_2003 REAL, _2004 REAL, _2005 REAL, _2006 REAL,'
                 '_2007 REAL, _2008 REAL, _2009 REAL, _2010 REAL);'
                 )
-# populate databases
+    # Initiate the master table for School life and GDP Joined
+    cur.execute('CREATE TABLE school_gdp (Country TEXT, Year INT, Total_Years INT, GDP REAL);')
+
+# populate tables
 with con:
-    cur.executemany('INSERT INTO school_life VALUES (?,?,?,?,?)', data_list)
-    cur.execute('INSERT INTO gdp '
-                '(country_name, _1999, _2000, _2001, _2002, '
-                '_2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) '
-                'VALUES ("' + line[0] + '","' + '","'.join(line[42:-5]) + '");')
+    cur.executemany('INSERT INTO school_life VALUES (?,?,?,?,?)', data_list)  # School Life Expectancy
+
+with open('/home/dssquared/PycharmProjects/Training/UN/API_NY.GDP.MKTP.CD_DS2_en_csv_v2.csv', 'rU') as inputFile:
+    next(inputFile)  # skip the first 4 lines
+    next(inputFile)
+    next(inputFile)
+    next(inputFile)
+    header = next(inputFile)
+    inputReader = csv.reader(inputFile)
+    for line in inputReader:
+        with con:
+            cur.execute('INSERT INTO gdp '
+                        '(country_name, _1999, _2000, _2001, _2002, '
+                        '_2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) '
+                        'VALUES ("' + line[0] + '","' + '","'.join(line[43:55]) + '");')
+con.close()
+
+"""
 # place table into a dataframe
 df = pd.read_sql_query("SELECT * FROM school_life", con, index_col="Country")
-con.close()
 
 # Basic stats
 men_mean = df['Men'].mean()
@@ -72,3 +92,4 @@ women_median = df['Women'].median()
 print("\nSchool Life Expectancy Statistics:\n")
 print("Men - Mean = {}    Median = {}".format(str(round(men_mean, 2)), str(men_median)))
 print("Women - Mean = {}    Median = {}\n".format(str(round(women_mean, 2)), str(women_median)))
+"""
