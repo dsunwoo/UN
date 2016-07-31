def688309235cdfe7daa7d4001b59eeec4f9d165
dsunwoo <sunwoo.daniel@gmail.com>
2016-07-30 19:44:13 -0500

    Clean up code and remove comments

--- UN_Scrapping.py
+++ UN_Scrapping.py
@@ -1,3 +1,13 @@
+"""
+Thinkful.com Training
+Unit4 - UN Education Data Scraping
+
+Use code below to scan through the table contents to find starting points:
+for index, row in enumerate(soup(<index string>)):
+    print(<index string>, index)
+    print(row)
+"""
+
 from bs4 import BeautifulSoup
 import requests
 import sqlite3 as lite
@@ -10,34 +20,22 @@ url = "https://raw.githubusercontent.com/dsunwoo/UN/master/Scrape.html"
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
 
-# Use code below to scan through the table contents to find starting points
-"""
-for index, row in enumerate(soup(<index string>)):
-    print(<index string>, index)
-    print(row)
-"""
 # Working table - Using above code, table index 11 is the starting point of the data
 main_table = soup('table')[11]
 header_row = main_table('tr')[7]
 
 # Setting up dictionaries
 d_data = {}
-# d_year = {}
-# d_total = {}
-# d_men = {}
-# d_women = {}
 
 # Scrape the data from table
-country_list = []
 data_list = []
 for x in range(8, 191):
     data_row = main_table('tr')[x]
     data = []
     for cell in data_row('td'):
         data.append(cell.get_text())
-    # Populate dictionaries with appropriate cell values
+    # Create a list of values for datatable
     data_list.append([data[0], int(data[1]), int(data[4]), int(data[7]), int(data[10])])
-    # country_list.append(data[0])
 
 # Setup database
 con = lite.connect('education.db')
@@ -53,17 +51,6 @@ with con:
                 )
 # populate database
 with con:
-    # Create an empty dataset
-    # for country in range(0, len(country_list)-1):
-    #    cur.execute("INSERT INTO school_life(Country) VALUES (?)", (country_list[country],))
-# Insert data into table
     cur.executemany('INSERT INTO school_life VALUES (?,?,?,?,?)', data_list)
-
-    # for x in range(0, len(data_list)-1):
-    #    cur.execute('UPDATE school_life SET Country = ' + data_list[x][0] +
-    #                ', Year = ' + data_list[x][1] +
-    #                ', Total = ' + data_list[x][4] +
-    #                ', Men = ' + data_list[x][7] +
-    #                ', Women = ' + data_list[x][10])
 con.close
 
