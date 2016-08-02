20bd540053e2bb145e39cac1d08e527d2e1952f6
dsunwoo <sunwoo.daniel@gmail.com>
2016-08-02 12:01:03 -0500

    Update UN_DataRead.py for csv import and include updated education.db

--- UN_DataRead.py
+++ UN_DataRead.py
@@ -11,9 +11,11 @@ cur = con.cursor()
 # place gdp table into a dataframe
 df = pd.read_sql_query("SELECT * FROM gdp", con)
 
-gdp_tall = []
+# gdp_tall = []
 
+"""
 for i in range(0, len(df)-1):
     for k,v in df.items():
         if k != 'country_name':
             gdp_tall.append([df['country_name'][i], k, df[k][i]])
+"""
